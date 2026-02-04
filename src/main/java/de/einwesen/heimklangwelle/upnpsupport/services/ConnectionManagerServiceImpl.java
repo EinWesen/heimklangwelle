30935dc87f3d72035b3c893ab554e104dfde09b3
EinWesen <einwesen@gmail.com>
2026-02-04 20:23:19 +0100

    Wildcard mediatypes and DNLA profiles

--- src/main/java/de/einwesen/heimklangwelle/upnpsupport/services/ConnectionManagerServiceImpl.java
+++ src/main/java/de/einwesen/heimklangwelle/upnpsupport/services/ConnectionManagerServiceImpl.java
@@ -8,16 +8,54 @@ import org.jupnp.support.model.ProtocolInfos;
 // UPNP annotations are inherited from parent
 public class ConnectionManagerServiceImpl extends ConnectionManagerService {
     
+	// TODO: I now have learned i wil need more than Connectionmanager, we need to rework this somehow
     public ConnectionManagerServiceImpl() {
         super(
                 new ProtocolInfos(), // sourceProtocolInfo (empty for renderer)
-                new ProtocolInfos(
-                        new ProtocolInfo(Protocol.HTTP_GET, "*", "video/*", 
-                            "DLNA.ORG_OP=01;DLNA.ORG_FLAGS=01700000000000000000000000000000"),
-                        new ProtocolInfo(Protocol.HTTP_GET, "*", "audio/*", 
-                            "DLNA.ORG_OP=01;DLNA.ORG_FLAGS=01700000000000000000000000000000")
-                    ) // sink
+                new ProtocolInfos( // sink
+                        // MP3
+                        new ProtocolInfo(
+                            Protocol.HTTP_GET,
+                            "*",
+                            "audio/mpeg",
+                            "DLNA.ORG_PN=MP3;DLNA.ORG_OP=01;DLNA.ORG_FLAGS=01700000000000000000000000000000"
+                        ),
+
+                        // AAC (ISO base media)
+                        new ProtocolInfo(
+                            Protocol.HTTP_GET,
+                            "*",
+                            "audio/mp4",
+                            "DLNA.ORG_PN=AAC_ISO;DLNA.ORG_OP=01;DLNA.ORG_FLAGS=01700000000000000000000000000000"
+                        ),
+
+                        // FLAC (DLNA 1.5)
+                        new ProtocolInfo(
+                            Protocol.HTTP_GET,
+                            "*",
+                            "audio/flac",
+                            "DLNA.ORG_PN=FLAC;DLNA.ORG_OP=01;DLNA.ORG_FLAGS=01700000000000000000000000000000"
+                        ),
+
+                        // OGG / Vorbis
+                        new ProtocolInfo(
+                            Protocol.HTTP_GET,
+                            "*",
+                            "audio/ogg",
+                            "DLNA.ORG_PN=OGG;DLNA.ORG_OP=01;DLNA.ORG_FLAGS=01700000000000000000000000000000"
+                        ),
+
+                        // H.264 + AAC in MP4
+                        new ProtocolInfo(
+                            Protocol.HTTP_GET,
+                            "*",
+                            "video/mp4",
+                            "DLNA.ORG_PN=AVC_MP4_BL_CIF15_AAC;DLNA.ORG_OP=01;DLNA.ORG_FLAGS=01700000000000000000000000000000"
+                        )                		
+                ) 
             );
     }
+    
+    
     
 }
