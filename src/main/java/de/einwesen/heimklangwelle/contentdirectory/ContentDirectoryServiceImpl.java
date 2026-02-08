c1b60b92af6d6e44debad4a26cbc5f3c47c0dcae
EinWesen <einwesen@gmail.com>
2026-02-08 13:10:32 +0100

    Implemented basic serving logic

--- src/main/java/de/einwesen/heimklangwelle/contentdirectory/ContentDirectoryServiceImpl.java
+++ src/main/java/de/einwesen/heimklangwelle/contentdirectory/ContentDirectoryServiceImpl.java
@@ -1,6 +1,16 @@
 package de.einwesen.heimklangwelle.contentdirectory;
 
-import org.jupnp.model.types.csv.CSV;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.jupnp.support.contentdirectory.AbstractContentDirectoryService;
 import org.jupnp.support.contentdirectory.ContentDirectoryErrorCode;
 import org.jupnp.support.contentdirectory.ContentDirectoryException;
@@ -8,74 +18,261 @@ import org.jupnp.support.contentdirectory.DIDLParser;
 import org.jupnp.support.model.BrowseFlag;
 import org.jupnp.support.model.BrowseResult;
 import org.jupnp.support.model.DIDLContent;
+import org.jupnp.support.model.DIDLObject;
+import org.jupnp.support.model.ProtocolInfo;
 import org.jupnp.support.model.Res;
 import org.jupnp.support.model.SortCriterion;
 import org.jupnp.support.model.container.StorageFolder;
+import org.jupnp.support.model.container.StorageSystem;
+import org.jupnp.support.model.container.StorageVolume;
+import org.jupnp.support.model.item.AudioItem;
+import org.jupnp.support.model.item.ImageItem;
+import org.jupnp.support.model.item.MusicTrack;
 import org.jupnp.support.model.item.VideoItem;
-import org.jupnp.util.MimeType;
-
-import de.einwesen.heimklangwelle.HeimklangServiceRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 //UPNP annotations are inherited from parent
 public class ContentDirectoryServiceImpl extends AbstractContentDirectoryService {
-
-
-	@Override
-	public CSV<String> getSearchCapabilities() {
-		// TODO Auto-generated method stub
-		return super.getSearchCapabilities();
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ContentDirectoryServiceImpl.class);
+	
+	private static final Map<String, ProtocolInfo> supportedMimetypes = new HashMap<>();
+	{
+		for (ProtocolInfo info : MediaServerConnectionManagerServiceImpl.SUPPORTED_PROTOCOLS) {
+			supportedMimetypes.put(info.getContentFormat(), info);
+		}		
 	}
-
-	@Override
-	public CSV<String> getSortCapabilities() {
-		// TODO Auto-generated method stub
-		return super.getSortCapabilities();
-	}
+	
+	private static Map<String, String> extensionMimetype = new HashMap<>(); 
+	{
+		extensionMimetype.put("mp3", "audio/mpeg");
+		extensionMimetype.put("flac", "audio/flac");
+		extensionMimetype.put("ogg", "audio/ogg");
+		extensionMimetype.put("m4a", "audio/mp4");
+		extensionMimetype.put("aac", "audio/aac");
+		extensionMimetype.put("mka", "audio/x-matroska");
+		extensionMimetype.put("mkv", "video/x-matroska");
+		extensionMimetype.put("mp4", "video/mp4");
+		extensionMimetype.put("m3u", "audio/x-mpegurl");
+		extensionMimetype.put("m3u8", "application/vnd.apple.mpegurl");
+		extensionMimetype.put("png", "image/png");
+		extensionMimetype.put("jpg", "image/jpeg");
+		extensionMimetype.put("gif", "image/gif");
+	}	
+	
+	private static final Encoder BASE64ENCODER = Base64.getUrlEncoder().withoutPadding();
+	private static final Decoder BASE64DECODER = Base64.getUrlDecoder();
 
 	@Override
 	public BrowseResult browse(String objectID, BrowseFlag browseFlag, String filter, long firstResult, long maxResults,
 			SortCriterion[] orderby) throws ContentDirectoryException {
 
-        DIDLContent didl = new DIDLContent();
+		DIDLContent didl = new DIDLContent();
 
-        // root container
-        if ("0".equals(objectID)) {
-        	StorageFolder  videos = new StorageFolder();
-            videos.setId("1");
-            videos.setParentID("0");
-            videos.setTitle("Videos");
-            videos.setRestricted(true);
-            //videos.setClazz(null)
-            didl.addContainer(videos);
-        }
+		try {
+			
 
-        // “Videos” container content
-        if ("1".equals(objectID)) {
-            VideoItem video = new VideoItem();
-            video.setId("100");
-            video.setParentID("1");
-            video.setTitle("Sample MP3");
-            
+			LOGGER.trace("%s \"%s\" (%s) %d - %d".formatted(browseFlag, objectID, filter, firstResult, maxResults));			
+			
+			// root container
+			if ("0".equals(objectID)) {
+				
+				switch (browseFlag) {
+					case METADATA:	// Return info about self
+						StorageSystem rootContainer = new StorageSystem();
+						rootContainer.setId("0");
+						rootContainer.setParentID("-1"); // no parent
+						rootContainer.setTitle(File.separator+File.separator);
+						rootContainer.setRestricted(true);
+						rootContainer.setSearchable(false);
+						rootContainer.setChildCount(File.listRoots().length);
+						didl.addContainer(rootContainer);				
+						break;
+					case DIRECT_CHILDREN: // Return children
+						for (File drive : File.listRoots()) {
+							StorageVolume  driveContainer = new StorageVolume();						
+							driveContainer.setId(encodeItemId(drive));
+							driveContainer.setParentID("0");
+							driveContainer.setTitle(drive.getAbsolutePath());
+							driveContainer.setRestricted(true);
+							driveContainer.setSearchable(false);
+							driveContainer.setChildCount(getChildCount(drive));
+							didl.addContainer(driveContainer);        		
+						}											
+						break;
+				default:
+					throw new IllegalArgumentException("browseFlag = " + browseFlag);
+				}        	
+								
+			} else {
+				
+				final File requestedObject = decodeItemId(objectID);
+				LOGGER.trace("Decoded:" + requestedObject.getAbsolutePath());
+				
+				if (requestedObject.exists() && requestedObject.canRead()) {
+					
+					switch (browseFlag) {
+						case METADATA:					
+							didl.addObject(getTypedDIDLObject(requestedObject));
+							break;
+						case DIRECT_CHILDREN:
+							if (requestedObject.isDirectory()) { 
+								final File[] children = requestedObject.listFiles();
+								if (children != null) {
+									for (File child : children) {
+										final DIDLObject typedDIDLObject = getTypedDIDLObject(child);
+										if (typedDIDLObject != null) {
+											didl.addObject(typedDIDLObject);										
+										}
+									}																	
+								}
+							} else {
+								throw new ContentDirectoryException(ContentDirectoryErrorCode.CANNOT_PROCESS, "Object is not an container"); 
+							}
+							break;
+						default:
+							throw new IllegalArgumentException("browseFlag = " + browseFlag);
+					}
+					
+					changeSystemUpdateID();
+					
+					
+					
+				} else {
+					throw new ContentDirectoryException(ContentDirectoryErrorCode.NO_SUCH_OBJECT, objectID);
+				}
+				
+			}
 
-            // Correct Res with String URI + ProtocolInfo
-            MimeType mimeType = new MimeType("audio", "mpeg");
-
-         // 2. Resource (URL zum Stream) hinzufügen
-         // Die URL muss für den Controller (z.B. Handy) erreichbar sein!
-         String streamUrl = HeimklangServiceRegistry.getContentServerBase() + "/sample.mp3";
-         Res res = new Res(mimeType, 1234567L, streamUrl); 
-         
-            video.addResource(res);
-            didl.addItem(video);
-        }
-
-        try {
-            String xml = new DIDLParser().generate(didl);
-            return new org.jupnp.support.model.BrowseResult(xml, didl.getCount(), didl.getCount());
-        } catch (Exception ex) {
-            throw new ContentDirectoryException(ContentDirectoryErrorCode.CANNOT_PROCESS, ex.getMessage());
-        }		
+			final String xml = new DIDLParser().generate(didl);
+			System.out.println(didl.getCount());
+			return new org.jupnp.support.model.BrowseResult(xml, didl.getCount(), didl.getCount());
+		
+		} catch (ContentDirectoryException c) {
+			throw c;
+		} catch (Throwable t) {
+			LOGGER.debug("Unexpected error", t);
+			throw new ContentDirectoryException(ContentDirectoryErrorCode.CANNOT_PROCESS.getCode(),t.toString());			
+		}
 		
 	}
+	
+	private static int getChildCount(File parent) {
+		int count = 0;
 
+		final File[] children = parent.listFiles();
+		if (children != null) {
+			for (File child : children) {
+				if (child.canRead()) {
+					if (child.isDirectory()) {
+						count += 1;
+					} else {
+						final String mimeTypeStr = getMimetype(child);
+						if (mimeTypeStr != null && supportedMimetypes.get(mimeTypeStr) != null) {
+							count += 1; 
+						}
+					}
+				}
+			}			
+		}
+		
+		return count;		
+	}	
+
+	private static DIDLObject getTypedDIDLObject(File fileObject) {
+		if (fileObject.isDirectory()) {
+			final StorageFolder folder =  new StorageFolder();
+			folder.setSearchable(false);
+			folder.setChildCount(getChildCount(fileObject));
+			return updateTypedObject(folder, fileObject, null);
+		}
+		
+		final String mimeTypeStr = getMimetype(fileObject);
+		
+		if (mimeTypeStr == null) return null;
+		
+		
+		final ProtocolInfo protocolinfo = supportedMimetypes.get(mimeTypeStr);
+		
+		if (protocolinfo == null) return null;		
+
+		if (mimeTypeStr.startsWith("audio/")) {
+			if (fileObject.getName().toLowerCase().endsWith("m3u") || fileObject.getName().toLowerCase().endsWith("m3u8")) {				
+				return updateTypedObject(new AudioItem(), fileObject, protocolinfo);
+			} else {
+				return updateTypedObject(new MusicTrack(), fileObject, protocolinfo);
+			}
+		} else if (mimeTypeStr.startsWith("video/")) {
+			return updateTypedObject(new VideoItem(), fileObject, protocolinfo);
+		} else if (mimeTypeStr.startsWith("image/")) {
+			return updateTypedObject(new ImageItem(), fileObject, protocolinfo);
+		} 
+		
+		return null;
+		
+	}
+		
+	
+	private static <D extends DIDLObject> D updateTypedObject(D dObj, File filedObject, ProtocolInfo protocolinfo) {
+		dObj.setId(encodeItemId(filedObject));
+		if (filedObject.getParentFile() != null) {
+			dObj.setParentID(encodeItemId(filedObject.getParentFile())); 			
+		} else {
+			dObj.setParentID("0");
+		}
+		dObj.setTitle(filedObject.getName());
+		dObj.setRestricted(true);
+
+		// Can only happen for directories
+		if (protocolinfo != null) {			
+			//final String streamUrl = HeimklangServiceRegistry.getContentServerBase() + "/sample.mp3";
+			dObj.addResource(new Res(protocolinfo, filedObject.length(), filedObject.getAbsoluteFile().toURI().toString()));
+		}
+		return dObj;
+	}
+	
+
+	
+	private static String getMimetype(File child) {
+		String mimeTypeStr = null;
+		try {
+			mimeTypeStr = Files.probeContentType(Path.of(child.getAbsolutePath()));
+		} catch (IOException e) {
+			LOGGER.warn("\"" + child.getAbsolutePath() + "\" : " + e.toString() );
+		}
+		
+		if (mimeTypeStr == null) {
+			final String ext = getFileExtension(child);
+			if (ext != null) {
+				mimeTypeStr = extensionMimetype.get(ext);
+			}
+		}
+		
+		if (mimeTypeStr != null) {
+			mimeTypeStr = mimeTypeStr.toLowerCase();
+		}
+		
+		return mimeTypeStr;
+	}
+	
+	private static String getFileExtension(File child) {
+	    final String fileName = child.getName();
+	    final int lastIndex = fileName.lastIndexOf('.');
+	    if (lastIndex > 0 && lastIndex < fileName.length() - 1) { // dot not at start or end
+	        return fileName.substring(lastIndex + 1).toLowerCase(); // optionally lowercase
+	    } else {
+	    	return null;	    	
+	    }
+	}
+	
+	
+	private static String encodeItemId(File fileObject) {		
+		// I used just teh absolute path before, and it worked semantically, but some device reject obcect with ids that contain : or \ it seems
+		return BASE64ENCODER.encodeToString(fileObject.getAbsolutePath().getBytes(StandardCharsets.UTF_8));
+	}
+	
+	private static File decodeItemId(String objectId) {
+		return new File(new String(BASE64DECODER.decode(objectId), StandardCharsets.UTF_8));
+	}
 }
--- src/main/java/de/einwesen/heimklangwelle/contentdirectory/MediaServerConnectionManagerServiceImpl.java
+++ src/main/java/de/einwesen/heimklangwelle/contentdirectory/MediaServerConnectionManagerServiceImpl.java
@@ -7,19 +7,28 @@ import de.einwesen.heimklangwelle.upnpsupport.AbstractConnectionManagerService;
 // UPNP annotations are inherited from parent
 public class MediaServerConnectionManagerServiceImpl extends AbstractConnectionManagerService {
     
+	public static ProtocolInfos SUPPORTED_PROTOCOLS = new ProtocolInfos(
+    		newDefaultServerProtocolInfo("audio/mpeg"),
+    		newDefaultServerProtocolInfo("audio/flac"),
+    		newDefaultServerProtocolInfo("audio/ogg"),
+    		newDefaultServerProtocolInfo("audio/mp4"), //m4a
+    		newDefaultServerProtocolInfo("audio/aac"),
+    		newDefaultServerProtocolInfo("audio/x-matroska"), //mka
+    		newDefaultServerProtocolInfo("video/x-matroska"),  
+    		newDefaultServerProtocolInfo("video/mp4"),                 		
+    		newDefaultServerProtocolInfo("audio/x-mpegurl"),
+    		newDefaultServerProtocolInfo("audio/mpegurl"),
+    		newDefaultServerProtocolInfo("application/vnd.apple.mpegurl"),
+    		newDefaultServerProtocolInfo("image/png"),
+    		newDefaultServerProtocolInfo("image/jpeg"),
+    		newDefaultServerProtocolInfo("image/gif")
+    );
+	
     public MediaServerConnectionManagerServiceImpl() {
-        super(
-                new ProtocolInfos(
-                		newDefaultServerProtocolInfo("audio/mpeg", "DLNA.ORG_PN=MP3"),
-                		newDefaultServerProtocolInfo("audio/flac", "DLNA.ORG_PN=FLAC"),
-                		newDefaultServerProtocolInfo("audio/ogg", "DLNA.ORG_PN=OGG"),
-                		newDefaultServerProtocolInfo("audio/mp4", "DLNA.ORG_PN=AAC_ISO"),
-                		newDefaultServerProtocolInfo("video/mp4", "DLNA.ORG_PN=AVC_MP4_BL_CIF15_AAC"),
-                		newDefaultServerProtocolInfo("video/mp4", "DLNA.ORG_PN=AVC_MP4_MP_SD_AAC"),
-                		newDefaultServerProtocolInfo("video/mp4", "DLNA.ORG_PN=AVC_MP4_MP_HD_720p_AAC")              
-                ), 
+        super(	SUPPORTED_PROTOCOLS, 
                 new ProtocolInfos() // sinkProtocolInfo (empty for server) 
             );
     }
         
 }
+
