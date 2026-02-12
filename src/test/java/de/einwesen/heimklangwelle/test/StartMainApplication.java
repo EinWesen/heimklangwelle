5e263b0643dc355174beb4ed27702e15372fceca
EinWesen <einwesen@gmail.com>
2026-02-12 20:14:27 +0100

    Introduce a starter class to be able to run from test classpath

---
+++ src/test/java/de/einwesen/heimklangwelle/test/StartMainApplication.java
@@ -0,0 +1,10 @@
+package de.einwesen.heimklangwelle.test;
+
+import de.einwesen.heimklangwelle.HeimklangStation;
+
+public class StartMainApplication {	
+	public static void main(String[] args) throws Exception {
+		System.out.println("Run main application form test classpath..");
+		HeimklangStation.main(args);
+	}
+}
