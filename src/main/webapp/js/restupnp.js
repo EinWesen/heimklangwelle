86e870d9b12a3698ebe92e184d5ba3f085e73e60
EinWesen <einwesen@gmail.com>
2026-02-15 18:34:02 +0100

    Support service action calls and subscription in the JS api

--- src/main/webapp/js/restupnp.js
+++ src/main/webapp/js/restupnp.js
@@ -61,3 +61,77 @@ export async function listDevices() {
 export async function browseContentDirectory(udn, objectId = "0") {
 	return apiFetch(`${BASE_REST_URL}/contentdirectory/${udn}/browse?objectId=${encodeURIComponent(objectId)}`);
 }
+
+/*
+{
+   "serviceId": "...",
+   "action": "...",
+   "inputArguments": {
+      "...": "...string reprenstation...",
+      "...": "...string reprenstation...",
+      ...
+   }
+}
+*/
+export async function callServiceAction(udn, callOptions) {
+
+	return new Promise((resolve, reject) => {
+		
+		apiFetch(`${BASE_REST_URL}/devices/${udn}/callAction`, {
+		  method: "POST",
+		  body: JSON.stringify(callOptions),
+		  headers: {
+		    "Content-type": "application/json; charset=UTF-8"
+		  }
+		}).then(apiResponse => {
+			// 201 CREATED = OK
+			// 202 ACCEPTED = VALID COMMAND FAILED
+			if (apiResponse.response.status == 201) {
+				resolve(apiResponse);
+			} else {
+				reject({
+					summary: data.message,
+				    isError: false,
+				    error: data.statusCode,
+				    response: apiResponse.response,
+				    data: apiResponse.data
+				});			
+			}
+		}).catch(errorInfo => reject(errorInfo));
+	
+	});
+		
+}
+
+
+
+export function createRendererEventSubcription(udn, instanceId, eventHandler) {
+    const es = new EventSource(`${BASE_REST_URL}/renderer/${udn}/subscribe`);
+		
+    // Attach the one-time listener
+	function rendererEventSubcriptionInitializeCallback(event) {        
+	    try {
+	        const propertyList = JSON.parse(event.data);
+	        if (Array.isArray(propertyList)) {
+		        // Register generic handler for each property/event name
+		        propertyList.forEach(propertyName => {
+					//console.debug(`Register listener for: ${propertyName}`);
+		            es.addEventListener(propertyName, eventHandler);
+		        });	            
+	        } else {
+	            console.error('X_PROPERTIES must be a JSON array:', propertyList);				
+			}
+			
+	    } catch (err) {
+	        console.error('Failed to parse X_PROPERTIES data:', event.data, err);			
+	    }
+		
+	    // Remove the X_PROPERTIES listener (one-time use)
+	    es.removeEventListener('X_PROPERTIES', rendererEventSubcriptionInitializeCallback);
+	    //console.log('Removed X_PROPERTIES listener');
+	    return;
+	}
+	
+    es.addEventListener('X_PROPERTIES', rendererEventSubcriptionInitializeCallback);
+    return es; // Return the EventSource in case the caller wants to close it later
+}
